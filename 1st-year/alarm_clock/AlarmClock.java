1869853ecf1e9666b55ca2550d97a16134d5ba5e
Cigdem-Yildiz <cigdemyildiz263@gmail.com>
2025-07-13 18:23:05 +0300

    Update AlarmClock.java
    getting alarm type input

--- 1st-year/alarm_clock/AlarmClock.java
+++ 1st-year/alarm_clock/AlarmClock.java
@@ -3,7 +3,22 @@ package alarm_clock;
 public class AlarmClock {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		LocalTime alarmTime = null;
+
+		while (alarmTime == null) {
+			System.out.print("Enter the alarm time(HH:MM:SS): ");
+			String inputTime = scanner.nextLine();
+
+			try {
+				alarmTime = LocalTime.parse(inputTime, formatter);
+			} catch (DateTimeParseException e) {
+				System.out.println("Wrong format, try again!");
+			}
+			
+			
+		}
 	}
 
 }
